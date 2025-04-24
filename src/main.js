a6ae5d989485f499c17224d58a3f972971f05eb3
daphnenea <daphnenea@gmail.com>
2025-04-24 14:47:46 -0700

    initial push-- light switch material from blender not showing up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Scene
 const scene = new THREE.Scene();
@@ -21,22 +22,52 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setClearColor(0x000000);
 
-// Add object
-const geometry = new THREE.BoxGeometry(5, 5, 5);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 // Add light
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(10, 10, 10);
-scene.add(light);
+// const light = new THREE.PointLight(0xffffff, 1);
+// light.position.set(10, 10, 10);
+// scene.add(light);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+// scene.add(ambientLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(ambientLight);
+
+// Load GLB Model from public folder
+
+const loader = new GLTFLoader();
+let model;
+loader.load(
+    '/LightSwitch_Off_2.glb',
+    (gltf) => {
+        model = gltf.scene;
+
+        model.traverse((child) => {
+            if (child.isMesh) {
+                console.log('Mesh name:', child.name);
+                console.log('Material:', child.material);
+            }
+        });
+
+        model.scale.set(10, 10, 10); // Adjust size as needed
+        model.position.set(5, 5, 5); // Adjust position if needed
+        model.rotation.y = Math.PI / -2;
+        scene.add(model);
+    }
+);
+
+// Add cube object
+
+const geometry = new THREE.BoxGeometry(5, 5, 5);
+const material = new THREE.MeshBasicMaterial({ color: 0x00fff0 });
+const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 // Animate
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    //model.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 animate();
